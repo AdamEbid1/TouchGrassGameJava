146f6704622146417e6dfdcfb7087f54470a9cdf
Adam Ebid <78551691+AdamEbid1@users.noreply.github.com>
2023-12-11 20:11:40 -0500

    Update TouchGrassApp.java

--- TouchGrassApp.java
+++ TouchGrassApp.java
@@ -16,11 +16,6 @@ public class TouchGrassApp extends  Application {
         launch(args);
     }
 
-    /*
-     * JavaFX is a Framework, and to use it we will have to
-     * respect its control flow!  To start the game, we need
-     * to call "launch" which will in turn call "start" ...
-     */
     @Override
     public void start(Stage primaryStage) throws IOException {
         this.model = new TouchGrassModel();
